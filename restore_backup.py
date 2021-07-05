71d106a476ddaa6213bfc24ab9e1595d6b3a597b
blinov.nstu <blinov.nstu@gmail.com>
2021-07-05 13:42:59 +0700

    rename backup-а

--- restore_backup.py
+++ restore_backup.py
@@ -8,7 +8,7 @@ def report_progress(line):
 def main():
     fdb.load_api('C:/Program Files/Firebird/Firebird_3_0/fbclient.dll')
     svc = fdb.services.connect(user='sysdba', password='masterkey')
-    svc.restore('backup.fbk', 'db.fdb', callback=report_progress)
+    svc.restore('Backup1.fbk', 'db.fdb', callback=report_progress)
 
 if __name__ == '__main__':
     main()
