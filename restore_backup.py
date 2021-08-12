1678389ccd2e6f9b69166170e96413eaedcf0348
blinov.nstu <blinov.nstu@gmail.com>
2021-08-12 23:05:39 +0700

    add DLL_PATH to config

--- restore_backup.py
+++ restore_backup.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 import fdb
 
+from config import DLL_PATH
+
+
 def report_progress(line):
+    """Help-ер для вывода процесса restore-а в консоль (можно без него)"""
     print(line)
 
 
 def main():
-    fdb.load_api('C:/Program Files/Firebird/Firebird_3_0/fbclient.dll')
+    fdb.load_api(DLL_PATH)
     svc = fdb.services.connect(user='sysdba', password='masterkey')
     svc.restore('Backup1.fbk', 'db.fdb', callback=report_progress)
+
 
 if __name__ == '__main__':
     main()
